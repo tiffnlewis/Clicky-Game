801c5d98975d88b33c79a6bbf8192f63a0f8a85d
Tiff L <tifftiffnlewis@gmail.com>
2019-04-24 22:56:26 -0500

    Updates

---
+++ precache-manifest.f6c5027f5140f100e4e34a60be7eb835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "3f5d070392879b3d4079",
+    "url": "/Clicky-Game/static/js/main.be43dce0.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3f5d070392879b3d4079",
+    "url": "/Clicky-Game/static/css/main.5c5f76e0.chunk.css"
+  },
+  {
+    "revision": "33709a206a9654bfeebe55c34db50b0f",
+    "url": "/Clicky-Game/index.html"
+  }
+];
