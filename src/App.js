035dd0171df08c1301d44bad51179d5e1f5abb6c
Tiff L <tifftiffnlewis@gmail.com>
2019-04-24 22:47:59 -0500

    updated images, fixed navbar and updated game logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import React, { Component } from "react";
 import Card from "./components/Card";
-import Footer from ".components/Footer";
-import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import cards from "./cards.json";
+import "./App.css";
 
+// Random shuffle
+function randomCards(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Jumbotron />
-        <Wrapper>
-          <Card />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  // Set this.state
+  state = {
+    cards,
+    currentScore: 0,
+    topScore: 0,
+    correctIncorrect: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      correctIncorrect: "You guessed correctly!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ correctIncorrect: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      correctIncorrect: "You guessed incorrectly!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledCards = randomCards(cards);
+    this.setState({ friends: shuffledCards });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Navbar
+          title="Doctor Who Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          correctIncorrect={this.state.correctIncorrect}
+        />
+
+        <Title>
+          Click on an image to earn points, but don't click on any more than once!
+        </Title>
+        <Container>
+          <Row>
+            {this.state.cards.map(card => (
+              <Column size="md-3 sm-6">
+                <Card
+                  key={card.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={card.id}
+                  image={card.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
 }
-    export default App;
+export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
-function Navbar(){
-        return (
-          <div className="App">
-            <header className="header">
-              <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <a className="navbar-brand" href="#">Tiffany's Clicky Game</a>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                  <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                      <a className="nav-link" href="#">Restart the game!</a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </header>
-          </div>
-            );
-          }
+import "./style.css";
+
+const Navbar = props => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/clicky-game/">{props.title}</a>
+    <div className="navbar-collapse collapse" id="navbarColor01">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-link" >{props.correctIncorrect}</li>
+      </ul>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-link" >Score - Top: {props.topScore} | Current: {props.score}</li>
+      </ul>
+    </div>
+  </nav>
+);
+
 export default Navbar;
